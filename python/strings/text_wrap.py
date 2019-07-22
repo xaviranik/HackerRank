a6c2d57915ba4ba4ed0b18e97df733ebc9be111d
xaviranik <xaviranik.me@gmail.com>
2019-07-22 18:06:08 +0600

    Create text_wrap.py

---
+++ python/strings/text_wrap.py
@@ -0,0 +1,10 @@
+import textwrap
+
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+
+
+string, max_width = input(), int(input())
+result = wrap(string, max_width)
+print(result)
