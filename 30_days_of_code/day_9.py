b35500ca4103b66206e57d18a8661f4c32284955
xaviranik <xaviranik.me@gmail.com>
2019-07-25 20:06:51 +0600

    Create day_9.py

---
+++ 30_days_of_code/day_9.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    result = factorial(n)
+    print(result)
